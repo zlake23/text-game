f65a15e9393fdd866156028f226cd8c9d8245f40
Zachary Lake <zlake23@gmail.com>
2020-04-10 22:07:12 -0700

    Added rooms list

---
+++ game.py
@@ -0,0 +1,73 @@
+rooms = [
+    {
+        "name": "lobby",
+        "description":
+            "As you stand in the lobby, a musty odor permeates the air. "
+            "A reception desk faces the entrance, coated in a layer of dust. "
+            "The double doors are chained closed with a padlock securing them shut. "
+            "A small room to the left has the word OFFICE printed in faded letters."
+    },
+    {
+        "name": "office",
+        "description1":
+            "The office door is locked. "
+            "Peering through the small window you see the usual office desk and chair. "
+            "The only thing worth noting is a safe set into the back wall.",
+
+        "description2":
+            "You use the key to open the office door. " 
+            "The wall safe is set to '0-0-0-0'.",
+
+        "description3":
+            "The safe makes an audible click as it unlocks. "
+            "Inside the safe is a small SILVER KEY."
+    },
+    {
+        "name": "short_hallway",
+        "description":
+            "The wallpaper is peeling and the carpet is stained along this short hallway. "
+            "To the left is ROOM 102. To the right is ROOM 103."
+    },
+    {
+        "name": "north_hallway",
+        "description": 
+            "A painting of a portrait can be seen at the end of the hall. "
+            "It has a large tear in it as if it had been slashed with a blade. "
+            "To your left is ROOM 104 and to the right is ROOM 105."
+        
+    },
+    {
+        "name": "south_hallway",
+        "description":
+            "The hallway stretches in front of you. "
+            "Looking left you see a short hallway. "
+            "On your right is ROOM 101."
+    },
+    {
+        "name": "room_101",
+        "description":
+            "An ordinary hotel room with faded walls."
+    },
+    {
+        "name": "room_102",
+        "description": 
+            "An ordinary hotel room with faded walls."
+    },
+    {
+        "name": "room_103",
+        "description": 
+            "An ordinary hotel room with faded walls.",
+    },
+    {
+        "name": "room_104",
+        "description": 
+            "An ordinary hotel room with faded walls.",
+    },
+    {
+        "name": "room_105",
+        "description": 
+            "An ordinary hotel room with faded walls. "
+            "On the nightstand is a BOOK with a tattered cover.",
+    },
+]
+print(rooms[9]['description'])
