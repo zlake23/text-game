aa21221eca6d9705afcad3e7314dd3dfe0056d08
Zachary Lake <zlake23@gmail.com>
2020-04-10 21:00:04 -0700

    Removed code and added import and if statements

--- start.py
+++ start.py
@@ -1,8 +1,20 @@
-print('This is the text-game file')
-
-def main():
-    print('Main function')
+import game
+import sys
 
 
 
-main()
+if len(sys.argv) > 1:
+    command = sys.argv[1]
+    if command == "start":
+        print("[Starting new game]")
+        if __name__ == "__main__":
+            game.main()
+    elif command == "quit":
+        print("Ending current game")
+        
+    else:
+        print('Please specify "start" or "quit"')
+else:
+    print('''Usage:
+        Start new game: python start.py start
+        Create new page: python start.py quit''')
